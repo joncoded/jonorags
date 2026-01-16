9481732a40844da0d0d6303e1fbc734299420fb8
joncoded <joncoded@gmail.com>
2026-01-15 20:43:12 -0500

    adding localization file

---
+++ local.py
@@ -0,0 +1,38 @@
+# jonorags by @joncoded (aka @jonchius)
+# dictionary of strings 
+# local.py 
+
+l = {
+  "en" : {
+    "ask_question" : "Ask a question", 
+    "bullets" : "Bullets",
+    "conversation_history" : "Conversation history",
+    "could_not_clear_namespace" : "Could not clear namespace", 
+    "files_uploaded" : "file(s) uploaded",
+    "loading_chunks" : "loading chunks to database... (this may take a while) 🙂",
+    "max_tokens" : "Maximum # of tokens",
+    "ner" : "Named entity recognition (NER)", 
+    "ner_ex" : "_Show a list of names found in the document(s)?_", 
+    "no_content_found" : "No content found",
+    "no_content_to_summarize" : "No content to summarize", 
+    "no_summary_could_be_made" : "No summary could be made",
+    "pages_processed" : "pages processed",
+    "process_click_when_ready" : "click 'Process documents' when ready",
+    "process_docs" : "process documents",     
+    "processed_file" : "processed file", 
+    "processing_docs" : "processing documents...please be patient, this may take some time!",
+    "reset_everything" : "Reset everything",
+    "reset_just_the_chat" : "Reset just the chat", 
+    "sentences" : "Sentences",
+    "sentiment_analysis" : "Sentiment analysis", 
+    "sentiment_analysis_ex" : "_Show the vibe of the document(s)?_",    
+    "settings_numbers" : "Summarize the following PDFs in this number of...",    
+    "splitting_documents" : "splitting document into chunks...",
+    "start_over" : "Start over", 
+    "submit_message" : "Submit", 
+    "summary" : "Summary", 
+    "summary_generating" : "Generating summary...",
+    "summary_of_documents" : "Summary of documents", 
+    "upload_instructions" : "Upload 1+ PDF files to summarize"
+  }
+}
